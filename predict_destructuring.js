76bcf55a6098bac82395df64173a73e6016a0d40
Tyler Duran <tymduran@gmail.com>
2022-05-02 13:28:04 -0700

    Updated

--- predict_destructuring.js
+++ predict_destructuring.js
@@ -18,7 +18,7 @@ const employee = {
 const { name: otherName } = employee;
 //Predict the output
 console.log(name); // ReferenceError: name is not defined
-console.log(otherName);
+console.log(otherName); // Elon
 
 
 // Problem #3
@@ -32,15 +32,15 @@ const password = '12345';
 const { password: hashedPassword } = person;  
 //Predict the output
 console.log(password); // 12345
-console.log(hashedPassword); // undefined
+console.log(hashedPassword); // undefined no password key in the person dictionary 
 
 
 // Problem #4 
 
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
-const [,first] = numbers;
-const [,,,second] = numbers;
-const [,,,,,,,,third] = numbers;
+const [,first] = numbers; // 2
+const [,,,second] = numbers; // 5
+const [,,,,,,,,third] = numbers; // 2
 //Predict the output
 console.log(first == second); // false 
 console.log(first == third); // true
